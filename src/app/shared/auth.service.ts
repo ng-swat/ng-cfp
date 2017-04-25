c7054326edcc5b23854beb64dcbe0d0b65176017
galta <gal.tamir@gmail.com>
2017-04-26 00:36:13 +0300

    Adding login's components: (forms validations & use service if form is valid)

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -3,15 +3,15 @@ import {RegisterInterface} from '../login/register.interface';
 import {PasswordRecoveryInterface} from '../login/password-recovery.interface';
 
 export class AuthService {
-  static login(loginForm: LoginInterface): void {
+  login(loginForm: LoginInterface): void {
     console.log('AuthService - login data: ', loginForm);
   }
 
-  static register(registerForm: RegisterInterface): void {
+  register(registerForm: RegisterInterface): void {
     console.log('AuthService - register data: ', registerForm);
   };
 
-  static passwordRecover(passwordRecoveryForm: PasswordRecoveryInterface): void {
+  passwordRecover(passwordRecoveryForm: PasswordRecoveryInterface): void {
     console.log('AuthService - passwordRecovery data: ', passwordRecoveryForm);
   };
 }
