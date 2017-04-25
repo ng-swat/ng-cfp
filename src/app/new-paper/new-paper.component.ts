4e1c8f0b5f081cd0b1a540f2f35af611e9d2e817
benorbiton <benornm@gmail.com>
2017-04-25 14:15:36 +0300

    NewPaper

--- src/app/new-paper/new-paper.component.ts
+++ src/app/new-paper/new-paper.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormArray, FormControl, FormGroup , Validators} from '@angular/forms';
 
 
 @Component({
@@ -6,12 +7,62 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './new-paper.component.html',
   styleUrls: ['./new-paper.component.css']
 })
+
+
 export class NewPaperComponent implements OnInit {
-
+  public ifSubmitted: boolean;
+  coSpeakersArr: Array<string> = ['benornm@gmail.com', 'yziv95@gmail.com'];
+  public newPaperFormInterface: FormGroup;
   constructor() {
+    this.ifSubmitted = false;
+    this.newPaperFormInterface = new FormGroup({
+      EventDetails: new FormGroup({
+        EventToSubmit: new FormControl('' , Validators.required),
+        Language: new FormControl('', Validators.required),
+      }),
+      SpeechDetails: new FormGroup({
+        Title: new FormControl(null, Validators.required),
+        Abstract: new FormControl(null , Validators.required),
+        Topic: new FormControl(null , Validators.required),
+      }),
+      SessionDetails: new FormGroup({
+        SessionType: new FormControl('', Validators.required),
+        Level: new FormControl('', Validators.required),
+        Length: new FormControl('', Validators.required),
+        Tags: new FormControl(),
+      }),
+      CoSpeakers: new FormArray([new FormControl()]) ,
+      Requirements: new FormGroup({
+        Requirements: new FormControl(),
+        Notes: new FormControl(null),
+      })
+    });
+    console.log(this.newPaperFormInterface.value);
   }
-
   ngOnInit() {
+  }
+  get EventDetails() {
+    return this.newPaperFormInterface.get('EventDetails');
+  }
+  get SpeechDetails() {
+    return this.newPaperFormInterface.get('SpeechDetails');
+  }
+  get SessionDetails() {
+    return this.newPaperFormInterface.get('SessionDetails');
+  }
+  get CoSpeakers() {
+    return this.newPaperFormInterface.get('CoSpeakers');
+  }
+  get Requirements() {
+    return this.newPaperFormInterface.get('Requirements');
+  }
+  public sendForm() {
+    this.ifSubmitted = true;
+    console.log(this.newPaperFormInterface.value);
+    console.log(this.EventDetails.get('EventToSubmit').valid);
+  }
+  public addToCoSpeakers () {
+    this.coSpeakersArr.push(this.CoSpeakers.value);
   }
 
 }
--- src/app/new-paper/new-paper.module.ts
+++ src/app/new-paper/new-paper.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewPaperComponent } from './new-paper.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   declarations: [NewPaperComponent] ,
   exports: [NewPaperComponent]
