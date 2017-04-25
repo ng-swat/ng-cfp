19e4baaf204f2dc1956cf83d298737e89d0c9415
Maxime Denoun <maxime_denoun@hotmail.com>
2017-04-25 17:57:19 +0300

    add routing navigation and fix few cosmetics in change pwd

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -11,11 +11,11 @@ export class NavigationComponent implements OnInit {
 
   constructor() {
     this.user = {file:'../assets/default_user.png', name:'Maxime'};
-    this.menus.push({id:1, name:'Profile', iconClass:'glyphicon glyphicon-user'});
-    this.menus.push({id:2, name:'Papers list', iconClass:'glyphicon glyphicon-list'});
-    this.menus.push({id:3, name:'New paper', iconClass:'glyphicon glyphicon-plus'});
-    this.menus.push({id:4, name:'Password', iconClass:'glyphicon glyphicon-lock'});
-    this.menus.push({id:5, name:'Logout', iconClass:'glyphicon glyphicon-remove'});
+    this.menus.push({id:1, name:'Profile', iconClass:'glyphicon glyphicon-user', route:'#'});
+    this.menus.push({id:2, name:'Papers list', iconClass:'glyphicon glyphicon-list', route:'#'});
+    this.menus.push({id:3, name:'New paper', iconClass:'glyphicon glyphicon-plus', route:'#'});
+    this.menus.push({id:4, name:'Password', iconClass:'glyphicon glyphicon-lock', route:'password'});
+    this.menus.push({id:5, name:'Logout', iconClass:'glyphicon glyphicon-remove', route:'#'});
   }
 
   ngOnInit() {
